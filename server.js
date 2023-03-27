345cb809cb50e8ec15f70a6d4e42719f37d98a69
Nazar Demchan <43341381+TearsOfFears@users.noreply.github.com>
2023-03-27 21:32:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const corsOptions = {
   optionSuccessStatus: 200
 }
 app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH, DELETE');
   res.header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
   next();
